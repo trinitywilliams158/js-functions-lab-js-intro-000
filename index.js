10cfb3a07e55fc718b96d70be5c660f70f0a4e20
trinitywilliams158 <trinitywilliams158@gmail.com>
2016-11-02 15:31:21 +0000

    First saved version of my nitrous code.

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var happyHolidays =function(){
+  return "Happy holidays!"
+};
+
+var happyHolidaysTo = function(you){
+  return "Happy holidays, you!"
+};
+
+var happyHolidayTo = function(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+};
+
+var holidayCountdown = function (holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
